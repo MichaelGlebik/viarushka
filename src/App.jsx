c3e07d04275c4384523b0988062c5a75cb135755
MichaelGlebik <michaelglebik@gmail.com>
2024-04-22 22:07:20 +0300

    wind of change

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <div className="main__content">
         <h1>ВИРТУАЛЬНАЯ РЕАЛЬНОСТЬ БЕЗ ПРОВОДОВ</h1>
         <p>Лучшее, что случалось с виртуальным миром в реальности. Новый компактный шлем без проводов и дорогих комьютеров.</p>
-        <button>Купить <img src={card} alt="card" /></button>
+        <button>Купить!<img src={card} alt="card" /></button>
         </div>
         <div className="main__pic"><img className="main__pic__img" src={pic} alt="boy" /></div>
       </main>
